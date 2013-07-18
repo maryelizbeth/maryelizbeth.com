d35e182946af22d243b9ccb4aa5997a91152c813
Mary Cutrali <mary.cutrali@gmail.com>
2013-07-18 15:35:31 -0400

    first commit

---
+++ maryelizbeth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maryelizbeth::Application.config.secret_token = '0f4b006b37a1363fee1f16cd08b43edbe600b4f4681b2d93cf8e955c6c1b16ade34e5fe9c219f6b6d76a326dbaf52e7720b72d386f0dda9d3db8b601011e8854'
---
+++ maryelizbeth/config/routes.rb
@@ -0,0 +1,5 @@
+Maryelizbeth::Application.routes.draw do
+
+  map.root :controller => 'static', :action => :index
+
+end
